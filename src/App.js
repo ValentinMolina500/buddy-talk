42b65e2c7ff42d9ced5d89679153091b98289d81
Valentin Molina <valentinmolina83@gmail.com>
2021-01-26 22:29:07 -0800

    Move api script

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { animateScroll } from "react-scroll";
 import './App.css';
-import CHAT_API from "./api/ChatAPI";
+
 import CC_LOGO from "./images/cc.png";
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+const PREFIX = "https://www.cleverbot.com/getreply";
+
+class ChatAPI {
+
+    /* Call back state for Cleverbot, needs to be passed back if set */
+    cs = null;
+
+    async getReply(input) {
+        /* Create base request */
+        let request = `${PREFIX}?key=${API_KEY}`;
+        
+        /* Add control state if present */
+        if (input !== "") {
+
+            /* Add input from user */
+            request += `&input=${encodeURIComponent(input)}`;
+        }
+
+        if (this.cs !== null) {
+            request += `&cs=${this.cs}`;
+        }
+
+        const response = await fetch(request);
+       
+        const json = await response.json();
+
+        /* Store control state */
+        this.cs = json.cs;
+
+        return json;
+    }
+}
+
+const CHAT_API = new ChatAPI();
 
 function App() {
   
@@ -192,5 +229,7 @@ function App() {
     </div>
   );
 }
+
+
 
 export default App;
